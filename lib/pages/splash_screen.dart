591419e222fbf567b77ae3a054ea59219c6dc4d4
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-04-25 09:48:31 +0530

    Update splash_screen.dart

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:baber_app/pages/home.dart';
-
-import 'package:baber_app/pages/login.dart'; // Import the next screen
+import 'package:baber_app/pages/login.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,12 +15,9 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    // Set a timer to navigate to the next screen after a few seconds
     Timer(const Duration(seconds: 6), () {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => Login(), // Navigate to the login screen
-        ),
+        MaterialPageRoute(builder: (context) => Login()),
       );
     });
   }
@@ -35,9 +31,9 @@ class _SplashScreenState extends State<SplashScreen> {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [
-              Colors.lightBlueAccent, // Light green color on one side
-              const Color.fromARGB(
-                  255, 166, 217, 169), // Darker green on the other side
+              Color(0xFFB91635), // Deep red
+              Color(0xff621d3c), // Burgundy
+              Color(0xff311937), // Dark purple
             ],
           ),
         ),
@@ -45,19 +41,15 @@ class _SplashScreenState extends State<SplashScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Display your logo or any other branding here
-              Center(
-                child: Image.asset(
-                  'images/logoo.png',
-                  width: 300, // Set width to 250 pixels
-                  height: 300, // Set height to 250 pixels
-                ),
+              Image.asset(
+                'images/logoo.png',
+                width: 300,
+                height: 300,
               ),
               const SizedBox(height: 20),
               CircularProgressIndicator(
-                color: Colors.red[
-                    300], // Set the progress indicator color to a matching brown
-              ), // Loading indicator
+                color: Colors.white, // Changed to white for better contrast
+              ),
             ],
           ),
         ),
